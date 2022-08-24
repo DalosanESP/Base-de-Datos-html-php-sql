512ade3924972cf3d003440cdf6ce757eee33420
DalosanESP <dalosanesp@gmail.com>
2022-08-24 14:50:46 +0200

    Actualizados formularios para que salgan las listas de los datos ya añadidos. Actualizado menu.

--- conexion_planeta.php
+++ conexion_planeta.php
@@ -5,102 +5,36 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>conexion_planeta</title>
-    <style type="text/css">
-     
-      table {
-        border: solid 2px #7e7c7c;
-        border-collapse: collapse;
-                     
-      }
-     
-      th, h1 {
-        background-color: #edf797;
-      }
-
-      td,
-      th {
-        border: solid 1px #7e7c7c;
-        padding: 2px;
-        text-align: center;
-      }
-
-
-    </style>
 </head>
 <body>
     
+<tr><td colspan="2" style='color: rgb(86, 126, 187);'><h1>Listado de planetas</h1></td></tr>
+<tr><td style='color: rgb(86, 126, 187);'><label>Planetas</label></td>
+	
 </body>
 </html>
 
+</tr>
+<!-- PARA MOSTRAR LISTAS -->
+<?php 
+include('conexion.php');
+session_start();
+$sql="SELECT * FROM planeta_origen";
+$result=mysqli_query($conexion,$sql);
 
-<?php
-//validamos datos del servidor
-$user = "root";
-$pass = "";
-$host = "localhost";
-
-//conetamos al base datos
-$connection = mysqli_connect($host, $user, $pass);
-
-//hacemos llamado al imput de formuario
-$nombre = $_POST["nombre"] ;
-
-//verificamos la conexion a base datos
-if(!$connection) 
-        {
-            echo "No se ha podido conectar con el servidor" . mysql_error();
-        }
-  else
-        {
-            echo "<b><h3>Hemos conectado al servidor</h3></b>" ;
-        }
-        //indicamos el nombre de la base datos
-        $datab = "mi_base";
-        //indicamos selecionar ala base datos
-        $db = mysqli_select_db($connection,$datab);
-
-        if (!$db)
-        {
-        echo "No se ha podido encontrar la Tabla";
-        }
-        else
-        {
-        echo "<h3>Tabla seleccionada:</h3>" ;
-        }
-        //insertamos datos de registro al mysql xamp, indicando nombre de la tabla y sus atributos
-        $instruccion_SQL = "INSERT INTO planeta_origen (nombre)
-                             VALUES ('$nombre')";
-                           
-                            
-        $resultado = mysqli_query($connection,$instruccion_SQL);
-
-        //$consulta = "SELECT * FROM tabla where id ='2'"; si queremos que nos muestre solo un registro en especifivo de ID
-        $consulta = "SELECT * FROM planeta_origen";
-        
-$result = mysqli_query($connection,$consulta);
-if(!$result) 
+while($mostrar=mysqli_fetch_array($result))
 {
-    echo "No se ha podido realizar la consulta";
-}
 echo "<table>";
 echo "<tr>";
 echo "<th><h1>id</th></h1>";
 echo "<th><h1>Nombre</th></h1>";
 echo "</tr>";
+	
+?>
+<tr> <td ><?php echo $mostrar['id'] ?></td>
+	    <td><?php echo $mostrar['nombre'] ?></td>
 
-while ($colum = mysqli_fetch_array($result))
- {
-    echo "<tr>";
-    echo "<td><h2>" . $colum['id']. "</td></h2>";
-    echo "<td><h2>" . $colum['nombre']. "</td></h2>";
-    echo "</tr>";
+</tr>
+<?php
 }
-echo "</table>";
-
-mysqli_close( $connection );
-
-   //echo "Fuera " ;
-   echo'<a href="formulario_planeta.html"> Volver Atrás</a>';
-
-
 ?>
---
+++ formulario_oficio.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>formulario oficio</title>
+  </head>
+  <body>
+    <h1>AÑADIR OFICIO</h1>
+    <form action="conexion_oficio.php" name="" method="POST">
+      <table border="0" align="center">
+        <tr>
+          <td>
+            Nombre
+          </td>
+          <td>
+            <label for="nombre"></label>
+            <input type="text" name="nombre" id="nombre" required  />
+          </td>
+        </tr>
+        <tr>
+          <td>&nbsp;</td>
+          <td>&nbsp;</td>
+        </tr>
+        <tr>
+          <td align="center">
+            <input
+              type="submit"
+              name="enviar"
+              id="enviar"
+              value="Enviar"
+            />
+          </td>
+          <td align="center">
+            <input type="reset" name="borrar" id="borrar" value="Restablecer" />
+          </td>
+        </tr>
+      </table>
+    </form>
+  </body>
+</html>
+<?php
+include('conexion.php');
+session_start();
+?>
+
+<tr><td colspan="2" style='color: rgb(86, 126, 187);'><h1>Listado de planetas</h1></td></tr>
+<tr><td style='color: rgb(86, 126, 187);'><label>Planetas</label></td>
+	
+</tr>
+<!-- PARA MOSTRAR LISTAS -->
+<?php 
+$sql="SELECT * FROM oficio";
+$result=mysqli_query($conexion,$sql);
+
+while($mostrar=mysqli_fetch_array($result))
+{
+echo "<table>";
+echo "<tr>";
+echo "<th><h1>id</th></h1>";
+echo "<th><h1>Nombre</th></h1>";
+echo "</tr>";
+	
+?>
+<tr> <td ><?php echo $mostrar['id'] ?></td>
+	    <td><?php echo $mostrar['nombre'] ?></td>
+
+</tr>
+<?php
+}
+?>
