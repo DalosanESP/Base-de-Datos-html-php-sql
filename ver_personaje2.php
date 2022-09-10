a573bac336ba6edf5fcbaec735e393dbc5b6270a
DalosanESP <dalosanesp@gmail.com>
2022-09-10 19:41:56 +0200

    Realizadas algunas configuraciones

---
+++ ver_personaje2.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ver Personajes</title>
+    <link rel="stylesheet" href="Estilos/personaje2.css">
+</head>
+<body>
+</body>
+</html>
+
+<?php
+include('conexion.php');
+session_start();
+$nombre = $_POST["nombre"] ;
+
+        //indicamos el nombre de la base datos
+        $datab = "mi_base";
+        //indicamos selecionar ala base datos
+        $db = mysqli_select_db($conexion,$datab);
+
+
+
+        //$consulta = "SELECT * FROM tabla where id ='2'"; si queremos que nos muestre solo un registro en especifivo de ID
+        $consulta = "SELECT * FROM personaje where nombre like '%$nombre%' or apellido like '%$nombre%'";;
+        
+$result = mysqli_query($conexion,$consulta);
+
+echo "<table cellspacing='10' >";
+echo "<tr>";
+echo "<th style='color:white';>Nombre</th>";
+echo "<th style='color:white';>Apellido</th>";
+echo "<th style='color:white';>Afiliacion</th>";
+echo "<th style='color:white';>Especie</th>";
+echo "<th style='color:white';>Plante de Origen</th>";
+echo "<th style='color:white';>Arma</th>";
+echo "<th style='color:white';>Oficio</th>";
+echo "<th style='color:white';>Nave</th>";
+echo "</tr>";
+
+while ($colum = mysqli_fetch_array($result))
+ {
+    echo "<tr>";
+    echo "<td style='color:white';>" . $colum['nombre']. "</td>";
+    echo "<td style='color:white';>" . $colum['apellido'] . "</td>";
+    echo "<td style='color:white';>" . $colum['afiliacion'] . "</td>";
+    echo "<td style='color:white';>" . $colum['especie'] . "</td>";
+    echo "<td style='color:white';>" . $colum['planeta_origen'] . "</td>";
+    echo "<td style='color:white';>" . $colum['arma'] . "</td>";
+    echo "<td style='color:white';>" . $colum['oficio'] . "</td>";
+    echo "<td style='color:white';>" . $colum['nave'] . "</td>";
+    echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close( $conexion );
+
+   //echo "Fuera " ;
+   echo'<a href="menu.html"> Volver al Menu</a>';
+
+
+?>
